16bb1d2eec01fdeedb69a9fce5d277233c19bc55
Patrick Sheriddan <sheridp2@gmail.com>
2019-03-26 13:18:58 -0700

    heroku deploy2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 app.use(express.static('./public'));
 
-server = app.listen(3000)
+server = app.listen(process.env.PORT || 3000)
 
 const io = require("socket.io")(server)
 
